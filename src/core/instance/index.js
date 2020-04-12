067abd84e83509098958a32a1c66e9c58805165e
dongaifeng <864857106@qq.com>
2020-04-12 15:13:17 +0800

    init

---
+++ src/core/instance/index.js
@@ -0,0 +1,25 @@
+import { initMixin } from './init'
+import { stateMixin } from './state'
+import { renderMixin } from './render'
+import { eventsMixin } from './events'
+import { lifecycleMixin } from './lifecycle'
+import { warn } from '../util/index'
+
+
+// Vue定义 混入模式  _init 函数出发  还有一些混入 init state， event  lifecycle render 
+function Vue (options) {
+  if (process.env.NODE_ENV !== 'production' &&
+    !(this instanceof Vue)
+  ) {
+    warn('Vue is a constructor and should be called with the `new` keyword')
+  }
+  this._init(options)
+}
+
+initMixin(Vue)
+stateMixin(Vue)
+eventsMixin(Vue)
+lifecycleMixin(Vue)
+renderMixin(Vue)
+
+export default Vue
